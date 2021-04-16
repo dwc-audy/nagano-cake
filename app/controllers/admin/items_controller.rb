62e8ed3d4cbdfdb56715f63d8220eaee8a9be7b6
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-04-16 16:28:46 +0000

    [add]商品新規投稿機能と一覧画面の作成（税率管理のみ未完） #17

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,9 +7,23 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
+  end
+
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :genre_id, :image)
+  end
+
 end
