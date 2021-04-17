2076b7f4549df03356262aa30cb73cd3c1eaec9b
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-17 13:06:11 +0000

    [update]edit #20

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,8 +4,32 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def update
+    @customer = Customer.find(current_customer.id)
+    @customer.update(customer_params)
+    render 'show'
   end
 
   def unsubscribe
   end
+
+  private
+
+  def customer_params
+    list = [
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :postal_code,
+      :address,
+      :telephone_number,
+      :email
+    ]
+    params.require(:customer).permit(list)
+  end
+
 end
