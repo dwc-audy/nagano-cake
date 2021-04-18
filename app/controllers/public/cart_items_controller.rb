2a836be26c60c0850a3c0eb56dc6b929e97d55dd
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-04-18 03:59:40 +0000

    [add]カート内商品画面作成　未完成 #33

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,15 @@
 class Public::CartItemsController < ApplicationController
+
+
   def index
+    @cart_item = CartItem.find(params[:item_id])
   end
+
+  def create
+    session[:cart_items] = [] unless session[:cart_items]
+    session[:cart_items] << params[:item_id]
+    redirect_to cart_items_path
+  end
+
+
 end
