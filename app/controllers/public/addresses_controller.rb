dbbb70c61270a28589ed935b97ad178c2f6fca88
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-19 01:03:12 +0000

    [update]schema file and create action for index view #31

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,6 +2,19 @@ class Public::AddressesController < ApplicationController
   def index
   end
 
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    render 'index'
+  end
+
   def edit
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
