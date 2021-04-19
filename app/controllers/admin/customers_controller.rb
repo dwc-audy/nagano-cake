467f1519564788ae0bdd7e24353f2c2d4bb6efb9
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-19 05:35:51 +0000

    [update]edit action #44

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,5 +8,29 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(customer.id)
+  end
+
+  private
+
+  def customer_params
+    list = [
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :postal_code,
+      :address,
+      :telephone_number,
+      :email,
+      :is_deleted
+    ]
+    params.require(:customer).permit(list)
   end
 end
