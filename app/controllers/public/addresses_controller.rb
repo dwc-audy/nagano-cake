e3afed3e814de998954d17c2ac439479f3215e76
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-19 02:29:03 +0000

    [update]refactoring for addresses_controller

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,4 +1,6 @@
 class Public::AddressesController < ApplicationController
+  before_action :set_address, only: [:destroy, :edit, :update]
+
   def index
     @addresses = current_customer.addresses
   end
@@ -7,26 +9,27 @@ class Public::AddressesController < ApplicationController
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     @address.save
-    render 'index'
+    redirect_to addresses_path
   end
 
   def destroy
-    @address = Address.find(params[:id])
     @address.destroy
     redirect_to addresses_path
   end
 
   def edit
-    @address = Address.find(params[:id])
   end
-  
+
   def update
-    @address = Address.find(params[:id])
     @address.update(address_params)
     redirect_to addresses_path
   end
 
   private
+
+  def set_address
+    @address = Address.find(params[:id])
+  end
 
   def address_params
     params.require(:address).permit(:postal_code, :address, :name)
