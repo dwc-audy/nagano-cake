90ebdc4d825ecf3759a7d817e88c8c0c57a4d809
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-20 00:41:17 +0000

    [update]refactoring sign_up_params position

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   layout 'public'
@@ -43,9 +43,18 @@ class Public::RegistrationsController < Devise::RegistrationsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    list = [
+        :last_name,
+        :first_name,
+        :last_name_kana,
+        :first_name_kana,
+        :postal_code,
+        :address,
+        :telephone_number
+        ]
+    devise_parameter_sanitizer.permit(:sign_up, keys: list)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
