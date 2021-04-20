c8c884155fe183ebc1cc01d8b2d5c589628a6cfa
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-20 17:15:28 +0000

    [update]customers_controller update to display error message #74

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -15,8 +15,11 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
-    @customer.update(customer_params)
-    redirect_to admin_customer_path(@customer.id)
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render 'edit'
+    end
   end
 
   private
