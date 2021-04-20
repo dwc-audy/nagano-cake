725886aa49ce8ba59d57a13763cf8ad68f59bbc5
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-20 00:28:50 +0000

    [update]routing after session and registration #54

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  
+
   layout 'admin'
 
   # GET /resource/sign_in
@@ -20,10 +20,18 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 end
