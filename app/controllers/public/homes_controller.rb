56870647123cabcbfb3a3a7efda914e20406cf7f
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-04-22 13:00:16 +0000

    [add]bxsliderの追加とabout画面の編集 #101

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,15 +1,16 @@
 class Public::HomesController < ApplicationController
 
 
-  
+
   layout 'public'
-  
+
   def top
     @genres = Genre.all
     @items = Item.limit(4).order(" created_at DESC ")
   end
-  
-  
+
+
   def about
+    @items = Item.all
   end
 end
