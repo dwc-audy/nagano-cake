c4cfc98c8997c4096a6ddc524e094ce766435ee6
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-22 10:32:16 +0000

    [update]homes#top view #97

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,15 +1,15 @@
 class Public::HomesController < ApplicationController
 
 
-  
+
   layout 'public'
-  
+
   def top
     @genres = Genre.all
-    @items = Item.limit(4).order(" created_at DESC ")
+    @items = Item.limit(8).order(" created_at DESC ")
   end
-  
-  
+
+
   def about
   end
 end
