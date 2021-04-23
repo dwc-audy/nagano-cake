f6961ea1aa0231f33ff81ac443322abc0aa2397a
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-23 17:51:47 +0000

    [update]admin/homes#top #110

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,14 +1,14 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
-  
+
   layout 'admin'
 
   def top
-    @orders = Order.all
+    @orders = Order.page(params[:page]).per(10)
   end
 
   def search
-    @orders = Order.where(customer_id: params[:format])
+    @orders = Order.where(customer_id: params[:format]).page(params[:page]).per(10)
     render 'top'
   end
 
